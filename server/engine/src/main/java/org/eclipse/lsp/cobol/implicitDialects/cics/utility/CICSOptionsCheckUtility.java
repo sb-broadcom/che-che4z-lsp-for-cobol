041a2bfb1d87b46fb0fed9fe8c986d59e61754b8
ilidio-lopes <ilidio.depinalopes@broadcom.com>
2024-11-29 16:30:39 +0100

    Implementation of WRITEQ statement in CICS (#2579)
    * Implementation of WRITEQ statement in CICS

--- server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSOptionsCheckUtility.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/implicitDialects/cics/utility/CICSOptionsCheckUtility.java
@@ -103,6 +103,9 @@ public class CICSOptionsCheckUtility {
         CICSReadNextReadPrevOptionsUtility.RULE_INDEX,
         new CICSReadNextReadPrevOptionsUtility(context, errors));
     optionsMap.put(
+        CICSWriteqOptionsCheckUtility.RULE_INDEX,
+        new CICSWriteqOptionsCheckUtility(context, errors));
+    optionsMap.put(
         CICSLinkOptionsCheckUtility.RULE_INDEX,
         new CICSLinkOptionsCheckUtility(context, errors));
     optionsMap.put(
